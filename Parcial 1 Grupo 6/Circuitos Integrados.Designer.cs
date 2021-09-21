aa424530acdbb432d245d7066b7e268a5dcbc21b
Joan Steven Gómez Contreras <joanstevengomezcontreras@outlook.com>
2021-09-21 09:57:27 -0600

    cambio

--- Parcial 1 Grupo 6/Circuitos Integrados.Designer.cs
+++ Parcial 1 Grupo 6/Circuitos Integrados.Designer.cs
@@ -176,7 +176,7 @@ namespace Parcial_1_Grupo_6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.BackColor = System.Drawing.Color.Teal;
             this.ClientSize = new System.Drawing.Size(593, 472);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
